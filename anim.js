1955338cf8c2695f3a5305e50ad3e8b57ac76fe0
Victor Tang <victortang1992@gmail.com>
2015-11-05 00:27:43 -0800

    add night check, add breaks to prevent skip to Tstorm

--- anim.js
+++ anim.js
@@ -16,18 +16,32 @@ var main = function($) {
               var icon = parsed_json['current_observation']['icon_url'];
               var wind_spd = parsed_json['current_observation']['wind_mph'];
               var wind_dir = parsed_json['current_observation']['wind_dir'];
+              var time = parsed_json['current_observation']['observation_time_rfc822'];
+              var Regexp = /\s(\d+):/g;
+              var hours = Regexp.exec(time)
               $("#location").html(location);
               $("#temperature").html(temp_f + "&deg;F");
               $("#weather").html(weather);
               $("#weatherimg").attr("src",icon);
               $("#wind_spd").html(wind_spd + "mph");
               $("#wind_dir").html(wind_dir);
-              switch(weather){
-                  case "Clear":document.querySelector("body").style.backgroundImage = "url('http://7-themes.com/data_images/out/13/6812493-sunflower-field-wallpaper.jpg')";
-                  case "Rain":document.querySelector("body").style.backgroundImage = "url('http://images4.alphacoders.com/831/83196.jpg')";
-                  case "Snow":document.querySelector("body").style.backgroundImage = "url('http://www.pageresource.com/wallpapers/wallpaper/comments-for-cousy-house-in-snowy-mountains.jpg')";
-                  case "Thunderstorms":document.querySelector("body").style.backgroundImage = "url('https://i.ytimg.com/vi/V_G2tjMipqg/maxresdefault.jpg')";
-                  default: return false;
+              if(6 < hours[1] && hours[1] < 18){
+                  switch(weather){
+                      case "Clear":document.querySelector("body").style.backgroundImage = "url('http://7-themes.com/data_images/out/13/6812493-sunflower-field-wallpaper.jpg')";break;
+                      case "Rain":document.querySelector("body").style.backgroundImage = "url('http://images4.alphacoders.com/831/83196.jpg')";break;
+                      case "Snow":document.querySelector("body").style.backgroundImage = "url('http://www.pageresource.com/wallpapers/wallpaper/comments-for-cousy-house-in-snowy-mountains.jpg')";break;
+                      case "Thunderstorms":document.querySelector("body").style.backgroundImage = "url('http://www.mrwallpaper.com/wallpapers/thunderstorm.jpg')";break;
+                      default: return false;
+                  };
+              }
+              else {
+                  switch(weather){
+                      case "Clear":document.querySelector("body").style.backgroundImage = "url('https://snapshotsbykats.files.wordpress.com/2011/11/020.jpg')";break;
+                      case "Rain":document.querySelector("body").style.backgroundImage = "url('http://newtopwallpapers.com/wp-content/uploads/2013/04/A-Rainy-Night-Background-For-Desktop.jpg')";break;
+                      case "Snow":document.querySelector("body").style.backgroundImage = "url('http://www.free-desktop-backgrounds.net/free-desktop-wallpapers-backgrounds/free-hd-desktop-wallpapers-backgrounds/565804859.jpg')";break;
+                      case "Thunderstorms":document.querySelector("body").style.backgroundImage = "url('http://www.pix123.com/freespins/NewsImages/587783_Thunder-Blue-Storm-at-Night.jpg')";break;
+                      default: return false;
+                  };
               }
           }
   });
