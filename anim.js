2b5cf9ecbd6727b7e19d1fa386a846d7e4dbc049
Victor Tang <victortang1992@gmail.com>
2015-11-05 00:03:34 -0800

    swapping works. clouds -> weather. Weather changing background

--- anim.js
+++ anim.js
@@ -1,36 +1,47 @@
-var main = function() {
+var main = function($) {
   
-jQuery(document).ready(function($) {
+
+  var the_good_stuff = {};
+
+
   $.ajax({
           url : "http://api.wunderground.com/api/eff03e7c66c8636b/conditions/q/autoip.json",
           dataType : "jsonp",
           success : function(parsed_json) {
+              the_good_stuff = parsed_json['current_observation'];
               var location = parsed_json['current_observation']['display_location']['full'];
               var temp_f = parsed_json['current_observation']['temp_f'];
               var temp_c = parsed_json['current_observation']['temp_c'];
-              var clouds = parsed_json['current_observation']['weather'];
+              var weather = parsed_json['current_observation']['weather'];
               var icon = parsed_json['current_observation']['icon_url'];
               var wind_spd = parsed_json['current_observation']['wind_mph'];
               var wind_dir = parsed_json['current_observation']['wind_dir'];
               $("#location").html(location);
               $("#temperature").html(temp_f + "&deg;F");
-              $("#cloudiness").html(clouds);
+              $("#weather").html(weather);
               $("#weatherimg").attr("src",icon);
               $("#wind_spd").html(wind_spd + "mph");
               $("#wind_dir").html(wind_dir);
+              switch(weather){
+                  case "Clear":document.querySelector("body").style.backgroundImage = "url('http://7-themes.com/data_images/out/13/6812493-sunflower-field-wallpaper.jpg')";
+                  case "Rain":document.querySelector("body").style.backgroundImage = "url('http://images4.alphacoders.com/831/83196.jpg')";
+                  case "Snow":document.querySelector("body").style.backgroundImage = "url('http://www.pageresource.com/wallpapers/wallpaper/comments-for-cousy-house-in-snowy-mountains.jpg')";
+                  case "Thunderstorms":document.querySelector("body").style.backgroundImage = "url('https://i.ytimg.com/vi/V_G2tjMipqg/maxresdefault.jpg')";
+                  default: return false;
+              }
           }
   });
-})
+
 
 
     $('.swap').click(function(){
         var current = $("#temperature").html();
         if(current.includes("F") == true){
             console.log("switch to C");
-            $("#temperature").html(temp_c + "&deg;C");
+            $("#temperature").html(the_good_stuff.temp_c + "&deg;C");
         }
         else{
-            $("#temperature").html(temp_f + "&deg;F");
+            $("#temperature").html(the_good_stuff.temp_f + "&deg;F");
         }
     }
 
